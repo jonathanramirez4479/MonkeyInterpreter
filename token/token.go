e3e67df59b9fe99ac5d7c77284de411ad0c9d04a
Jonathan Ramirez <jonathanramirez4479@gmail.com>
2025-12-30 20:15:43 -0800

    Chapter 1.3 Lexer

---
+++ token/token.go
@@ -0,0 +1,35 @@
+// Package token
+package token
+
+type TokenType string
+
+type Token struct {
+	Token   TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add foobar, x, y, ...
+	INT   = "INT"   // 123456
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
